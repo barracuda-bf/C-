d5a2ab1b42e7741ce49941f143395c177faa5aed
ss <barracudablackfire@mail.ru>
2017-06-16 17:48:23 +0600

    12

---
+++ Labs87/Task6/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("B: ");
+            int B = int.Parse(Console.ReadLine());
+            Console.Write("E: ");
+            int E = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Данные:");
+            var array = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            if (B > E) {
+                for (int i = B; i <= E; i++)
+                    Console.Write(array[i] + " "); 
+                
+            } else { 
+            for (int i = E; i <= B; i--)
+                    Console.Write(array[i] + " ");
+            }
+            //Console.ReadKey();
+ 
+        }
+    }
+}
